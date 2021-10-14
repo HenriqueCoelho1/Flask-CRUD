f37fd2c32315a03253184f1671b6ef070b434787
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-10-13 21:11:21 -0300

    Creating

--- backend/app.py
+++ backend/app.py
@@ -1,33 +1,63 @@
-from flask import Flask, render_template, session, redirect, url_for, flash
+from flask import Flask, render_template, session, redirect, url_for, flash, Response, request
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+
+
 app = Flask(__name__)
-
-
 bootstrap = Bootstrap(app)
-moment = Moment(app)
-app.config['SECRET_KEY'] = 'hard to guess string'
+ENV = "dev"
+if ENV == "dev":
+    app.debug = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:1234@localhost:5432/teste"
+else:
+    app.debug = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = ""
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 
-class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+class FeedBack(db.Model):
+    __tablename__ = "feedback"
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.String(200), unique=True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self, customer, dealer, rating, comments):
+        self.customer = customer
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Look like you have changed your name')
-        session['name'] = form.name.data
-        return redirect(url_for('index'))
-    return render_template("index.html", form=form, name=session.get('name'))
+    return render_template("index.html")
+
+
+@app.route('/submit', methods=["POST"])
+def submit_form():
+    if request.method == "POST":
+        customer = request.form["customer"]
+        dealer = request.form["dealer"]
+        rating = request.form["rating"]
+        comments = request.form["comments"]
+        # print(customer, dealer, rating, comments)
+        if customer == "" or dealer == "":
+            return render_template("index.html", message="Please enter required fields")
+        if db.session.query(FeedBack).filter(FeedBack.customer == customer).count() == 0:
+            data = FeedBack(customer, dealer, rating, comments)
+            db.session.add(data)
+            db.session.commit()
+            return render_template("success.html")
+        else:
+            return render_template("index.html", message="This name is already exist")
 
 
 @app.route('/user/<name>')
@@ -43,3 +73,7 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
+if __name__ == "__main__":
+    app.run()
